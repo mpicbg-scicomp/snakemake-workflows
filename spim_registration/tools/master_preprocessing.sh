908809c561d1b6170f7c7d11f8243ee1b41457c8
Christopher Schmied <schmied@mpi-cbg.de>
2015-07-15 21:20:35 +0200

    Added sysconfcpus
    Tested single channel tif with LOCI bioformats

--- spim_registration/tools/master_preprocessing.sh
+++ spim_registration/tools/master_preprocessing.sh
@@ -19,7 +19,7 @@
 #	channels are 0,1 etc
 # 
 #===============================================================================
-image_file_directory="/projects/pilot_spim/Christopher/test_pipeline/dual_channel/tif_onefile/"
+image_file_directory="/projects/pilot_spim/Christopher/test_pipeline/single_channel/tif/"
 
 # --- jobs directory -----------------------------------------------------------
 job_directory="/projects/pilot_spim/Christopher/snakemake-workflows/spim_registration/tools/"
@@ -29,7 +29,7 @@ job_directory="/projects/pilot_spim/Christopher/snakemake-workflows/spim_registr
 # Important: For renaming and resaving .czi files the first .czi file has to
 # carry the index (0)
 #-------------------------------------------------------------------------------
-first_czi_name="2015-02-20_LZ2_Stock48_Stock58.czi"
+first_czi_name="2015-02-21_LZ1_Stock68_3.czi"
 timepoints="`seq 0 1`" # number of time points format: "`seq 0 1`"
 angles="1 2 3 4 5" # angles format: "1 2 3"
 pad="1"		# for padded zeros
