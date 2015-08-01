b0c35073e8ff67992d577a362a556a5df67bb6b1
Christopher Schmied <schmied@mpi-cbg.de>
2015-08-02 00:25:42 +0200

    Small adjustment for config yaml and master preprocessing

--- spim_registration/tools/master_preprocessing.sh
+++ spim_registration/tools/master_preprocessing.sh
@@ -19,21 +19,21 @@
 #	channels are 0,1 etc
 # 
 #===============================================================================
-image_file_directory="/projects/pilot_spim/Christopher/test_pipeline/single_channel/tif/"
+image_file_directory="/projects/pilot_spim/Christopher/2015-04-22_LZ2_w1118_DualChannel/"
 
 # --- jobs directory -----------------------------------------------------------
 job_directory="/projects/pilot_spim/Christopher/snakemake-workflows/spim_registration/tools/"
 #-------------------------------------------------------------------------------
-# Resaving, Renaming files and Splitting: General
+# Resaving, Renaming files: General
 #
 # Important: For renaming and resaving .czi files the first .czi file has to
 # carry the index (0)
 #-------------------------------------------------------------------------------
-first_czi_name="2015-02-21_LZ1_Stock68_3.czi"
-timepoints="`seq 0 1`" # number of time points format: "`seq 0 1`"
+first_czi_name="2015-04-22_LZ2_w1118_DualChannel_2.czi"
+timepoints="`seq 0 74`" # number of time points format: "`seq 0 1`"
 angles="1 2 3 4 5" # angles format: "1 2 3"
 pad="1"		# for padded zeros
-first_timepoint="0"	# Starts with 0
+first_timepoint="4"	# First .tif needs to start with 0 for workflow
 #-------------------------------------------------------------------------------
 # Fiji settings
 #-------------------------------------------------------------------------------
