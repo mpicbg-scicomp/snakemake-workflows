4371726441ad1f50e0ffdf4c4d98060a0e5a20b9
Christopher Schmied <schmied@mpi-cbg.de>
2016-08-22 22:55:22 +0200

    Added export of .h5 luxendo files to .tif

---
+++ spim_registration/tools/luxendo_resave/create_lux_resave.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# path of master file
+source ../master_preprocessing.sh
+
+# creates directory for job files if not present
+mkdir -p ${jobs_luxendo}
+
+# splits up resaving into 1 job per .czi file and writes the given parameters
+# into the job file
+tif_timepoint=${first_timepoint}
+
+for timepoint in ${timepoints}
+
+do
+	for angle in ${angles}
+	do
+		job="$jobs_luxendo/resave-${timepoint}-${angle}.job"
+		echo $job
+		echo "${XVFB_RUN} ${sysconfcpus} ${Fiji_lux} \
+			-Ddir=${image_file_directory} \
+			-Dangledir=${view_directory} \
+			-Dtimepoint=${timepoint} \
+			-Dangle=${angle} \
+			-Dpad=${pad} \
+			-- --no-splash ${luxendo_resave}" >> "${job}"
+		chmod a+x "${job}"
+	done
+timepoint=$((${timepoint} + 1))
+
+done
